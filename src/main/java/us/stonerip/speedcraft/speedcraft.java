91c81a362a094c98b506dfad11a9bb2da4e7017b
stonerip <booguyster@gmail.com>
2014-08-23 21:40:51 -0500

    :floppy_disk: Auto Commit 2014-08-23_0940 PM :floppy_disk:

--- src/main/java/us/stonerip/speedcraft/speedcraft.java
+++ src/main/java/us/stonerip/speedcraft/speedcraft.java
@@ -28,7 +28,7 @@ public final class speedcraft extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("scstart")) { // If the player typed /basic then do the following...
     		getLogger().info("Starting new run!");
-    		sender.sendMessage("[SpeedCraft] Starting a new run!");
+    		sender.sendMessage("&2[SpeedCraft] &8Starting a new run!&r");
     		Player p = (Player)sender;
     		ByteArrayDataOutput out = ByteStreams.newDataOutput();
     		out.writeUTF("Connect");
